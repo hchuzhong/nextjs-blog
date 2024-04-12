38a74d8926b48a7d4d833f0f7fafc609bbceac51
hchuzhong <hchuzhong@163.com>
2024-04-12 10:06:12 +0800

    use outside images

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,11 @@
+import Image from "next/image";
 
 export default function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <div>
+      <div className="relative w-full h-[500px]">
+        <Image src="https://images.pexels.com/photos/12704502/pexels-photo-12704502.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt="About page" fill />
+      </div>
+    </div>
   );
 }
